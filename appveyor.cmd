65c9cd213fe6bbe62af177d9b5e4621f46a94532
maniglia <a.klede@hotmail.it>
2020-06-07 11:05:47 +0200

    Restore MakeSfxCA and SfxCA projects (first review)

--- appveyor.cmd
+++ appveyor.cmd
@@ -3,7 +3,10 @@
 
 nuget restore
 
-msbuild -p:Configuration=Release
+msbuild -p:Configuration=Release;Platform=x86;PlatformToolset=v141
+msbuild -p:Configuration=Release;Platform=x64;PlatformToolset=v141
+
+msbuild -p:Configuration=Release -t:PackNativeNuget src\Tools\SfxCA\SfxCA.vcxproj
 
 @popd
 @endlocal
