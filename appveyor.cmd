a1a8cf88dec6b6a9ec1df511b81069145b1b252a
maniglia <a.klede@hotmail.it>
2020-06-09 17:35:59 +0200

    WixToolset.Dtf.MSBuild

--- appveyor.cmd
+++ appveyor.cmd
@@ -1,7 +1,13 @@
 @setlocal
 @pushd %~dp0
+@set _P=%~dp0build\Release\publish
 
 nuget restore
+
+dotnet publish -c Release -o %_P%\WixToolset.Dtf.MSBuild\net461\ -f net461 src\Tools\MakeSfxCA 
+dotnet publish -c Release -o %_P%\WixToolset.Dtf.MSBuild\netcoreapp2.1\ -f netcoreapp2.1 src\Tools\MakeSfxCA
+
+dotnet pack -c Release src\WixToolset.Dtf.MSBuild
 
 msbuild -p:Configuration=Release
 
