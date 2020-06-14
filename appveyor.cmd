b266268e629953871c4f391cf2c26028d1ac2866
maniglia <a.klede@hotmail.it>
2020-06-14 16:50:38 +0200

    WixToolset.Dtf.MSBuild CR 1

--- appveyor.cmd
+++ appveyor.cmd
@@ -7,15 +7,30 @@
 nuget restore
 
 msbuild -p:Configuration=%_C%
-msbuild %_S%Tools\SfxCA\SfxCA.vcxproj -p:Configuration=%_C% -p:Platform="x64" -p:OutputPath="%_P%\WixToolset.Dtf.MSBuild\\"
-msbuild %_S%Tools\SfxCA\SfxCA.vcxproj -p:Configuration=%_C% -p:Platform="x86" -p:OutputPath="%_P%\WixToolset.Dtf.MSBuild\\"
+
+msbuild %_S%Tools\SfxCA\SfxCA.vcxproj -p:Configuration=%_C% -p:Platform="x64" -p:OutputPath="%_P%\WixToolset.Dtf.MSBuild\tools\\"
+msbuild %_S%Tools\SfxCA\SfxCA.vcxproj -p:Configuration=%_C% -p:Platform="x86" -p:OutputPath="%_P%\WixToolset.Dtf.MSBuild\tools\\"
+
 msbuild %_S%Tools\MakeSfxCA\MakeSfxCA.csproj -p:Configuration=%_C% -p:TargetFramework="net461" -p:OutputPath="%_P%\WixToolset.Dtf.MSBuild\tools\net461"
 msbuild %_S%Tools\MakeSfxCA\MakeSfxCA.csproj -p:Configuration=%_C% -p:TargetFramework="netcoreapp2.1" -p:OutputPath="%_P%\WixToolset.Dtf.MSBuild\tools\netcoreapp2.1"
-msbuild %_S%WixToolset.Dtf.MSBuild\WixToolset.Dtf.MSBuild.csproj -p:OutputPath="%_P%\WixToolset.Dtf.MSBuild"
+
+msbuild %_S%WixToolset.Dtf.Compression\WixToolset.Dtf.Compression.csproj -p:Configuration=%_C% -p:OutputPath="%_P%\WixToolset.Dtf.MSBuild\tools
+msbuild %_S%WixToolset.Dtf.Compression.Cap\WixToolset.Dtf.Compression.Cap.csproj -p:Configuration=%_C% -p:OutputPath="%_P%\WixToolset.Dtf.MSBuild\tools
+msbuild %_S%WixToolset.Dtf.Compression.Zip\WixToolset.Dtf.Compression.Zip.csproj -p:Configuration=%_C% -p:OutputPath="%_P%\WixToolset.Dtf.MSBuild\tools
+msbuild %_S%WixToolset.Dtf.Resources\WixToolset.Dtf.Resources.csproj -p:Configuration=%_C% -p:OutputPath="%_P%\WixToolset.Dtf.MSBuild\tools
+msbuild %_S%WixToolset.Dtf.WindowsInstaller\WixToolset.Dtf.WindowsInstaller.csproj -p:Configuration=%_C% -p:OutputPath="%_P%\WixToolset.Dtf.MSBuild\tools
+msbuild %_S%WixToolset.Dtf.WindowsInstaller.Linq\WixToolset.Dtf.WindowsInstaller.Linq.csproj -p:Configuration=%_C% -p:OutputPath="%_P%\WixToolset.Dtf.MSBuild\tools
+msbuild %_S%WixToolset.Dtf.WindowsInstaller.Package\WixToolset.Dtf.WindowsInstaller.Package.csproj -p:Configuration=%_C% -p:OutputPath="%_P%\WixToolset.Dtf.MSBuild\tools
+
+msbuild %_S%WixToolset.Dtf.MSBuild\WixToolset.Dtf.MSBuild.csproj -p:Configuration=%_C% -p:OutputPath="%_P%\WixToolset.Dtf.MSBuild"
 
 msbuild %_S%WixToolset.Dtf.Compression\WixToolset.Dtf.Compression.csproj -p:Configuration=%_C% -t:CustomBuild
 msbuild %_S%WixToolset.Dtf.Compression.Cab\WixToolset.Dtf.Compression.Cab.csproj -p:Configuration=%_C% -t:CustomBuild
+msbuild %_S%WixToolset.Dtf.Compression.Zip\WixToolset.Dtf.Compression.Zip.csproj -p:Configuration=%_C% -t:CustomBuild
 msbuild %_S%WixToolset.Dtf.Resources\WixToolset.Dtf.Resources.csproj -p:Configuration=%_C% -t:CustomBuild
+msbuild %_S%WixToolset.Dtf.WindowsInstaller\WixToolset.Dtf.WindowsInstaller.csproj -p:Configuration=%_C% -t:CustomBuild
+msbuild %_S%WixToolset.Dtf.WindowsInstaller.Linq\WixToolset.Dtf.WindowsInstaller.Linq.csproj -p:Configuration=%_C% -t:CustomBuild
+msbuild %_S%WixToolset.Dtf.WindowsInstaller.Package\WixToolset.Dtf.WindowsInstaller.Package.csproj -p:Configuration=%_C% -t:CustomBuild
 
 msbuild %_S%WixToolset.Dtf.MSBuild\WixToolset.Dtf.MSBuild.csproj -target:Pack -p:Configuration=%_C%
 
