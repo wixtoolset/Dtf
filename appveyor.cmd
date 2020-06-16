df5e63c115e6020323fbcfdb382dd02c85c5792f
maniglia <kSociety85@gmail.com>
2020-06-16 08:51:41 +0200

    WixToolset.Dtf.MSBuild (#4)
    Add WixToolset.Dtf.MSBuild nuget package.

--- appveyor.cmd
+++ appveyor.cmd
@@ -1,9 +1,20 @@
 @setlocal
 @pushd %~dp0
+@set _C=Release
+@set _P=%~dp0build\%_C%\publish
 
 nuget restore
 
-msbuild -p:Configuration=Release
+msbuild -p:Configuration=%_C% -t:CustomBuild
+
+msbuild .\src\Tools\SfxCA\SfxCA.vcxproj -p:Configuration=%_C% -p:Platform="x64" -p:OutputPath="%_P%\WixToolset.Dtf.MSBuild\tools\\"
+msbuild .\src\Tools\SfxCA\SfxCA.vcxproj -p:Configuration=%_C% -p:Platform="x86" -p:OutputPath="%_P%\WixToolset.Dtf.MSBuild\tools\\"
+
+msbuild .\src\Tools\MakeSfxCA\MakeSfxCA.csproj -p:Configuration=%_C% -p:TargetFramework="net461" -p:OutputPath="%_P%\WixToolset.Dtf.MSBuild\tools\net461"
+msbuild .\src\Tools\MakeSfxCA\MakeSfxCA.csproj -p:Configuration=%_C% -p:TargetFramework="netcoreapp2.1" -p:OutputPath="%_P%\WixToolset.Dtf.MSBuild\tools\netcoreapp2.1"
+
+msbuild .\src\WixToolset.Dtf.MSBuild\WixToolset.Dtf.MSBuild.csproj -p:Configuration=%_C% -p:OutputPath="%_P%\WixToolset.Dtf.MSBuild"
+msbuild .\src\WixToolset.Dtf.MSBuild\WixToolset.Dtf.MSBuild.csproj -target:Pack -p:Configuration=%_C%
 
 @popd
 @endlocal
--- src/Tools/MakeSfxCA/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-// Copyright (c) .NET Foundation and contributors. All rights reserved. Licensed under the Microsoft Reciprocal License. See LICENSE.TXT file in the project root for full license information.
-
-using System.Reflection;
-
-[assembly: AssemblyDescription("Custom action packaging tool.")]
-[assembly: AssemblyCompany("WiX Toolset Team")]
-[assembly: AssemblyProduct("WiX Toolset")]
-[assembly: AssemblyCopyright("Copyright (c) .NET Foundation and contributors. All rights reserved.")]
-
